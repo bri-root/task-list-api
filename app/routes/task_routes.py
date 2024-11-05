1d93b7b7977bf0af16fe16c7cd37557cca366ac5
Brianna Root <brianna.k.root@gmail.com>
2024-11-05 17:02:55 +1300

    Added POST request for /tasks

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,3 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request
+from app.models.task import Task
+from ..db import db
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body["completed_at"]
+
+    new_task = Task(title=title, description=description, completed_at=completed_at)
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+        "id": new_task.id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "completed_at": completed_at
+    }
+    return response, 201
