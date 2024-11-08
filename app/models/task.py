61b99cf0c6124499da361d77fd1c6eeffb6d882f
Brianna Root <brianna.k.root@gmail.com>
2024-11-09 06:56:39 +1300

    restarted migration folder and updated to_dict

--- app/models/task.py
+++ app/models/task.py
@@ -16,13 +16,15 @@ class Task(db.Model):
     goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
     
     def to_dict(self):
-        return {
+        task_dict = {
             "id":self.id, 
             "title":self.title,
             "description":self.description,
             "is_complete":self.completed_at is not None,
-            "goal_id": self.goal_id
         }
+        if self.goal_id:
+            task_dict["goal_id"] = self.goal_id
+        return task_dict
 
     @classmethod
     def from_dict(cls, task_data):
---
+++ migrations/versions/807eea87a6b5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 807eea87a6b5
+Revises: 
+Create Date: 2024-11-09 06:42:05.787657
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '807eea87a6b5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
